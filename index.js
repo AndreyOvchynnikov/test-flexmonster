788454214b6c289623f789ac1d273d60fd08e1c7
AndreyOvchynnikov <andrey272015@gmail.com>
2023-11-10 16:52:50 +0200

    update

--- index.js
+++ index.js
@@ -8,13 +8,19 @@ refs.form.addEventListener('submit', handleSubmit);
 function handleSubmit(event) {
     event.preventDefault();
     const { email, password, firstName, lastName, companyName } = event.target.elements;
+    if (!validateEmail(email.value)) {
+        alert('Email is invalid!') 
+        return
+        }
+    
+
     const formData = {
         email: email.value,
         password: password.value,
         firstName: firstName.value,
         lastName: lastName.value,
         companyName: companyName.value,
-        }
+    }
     const formDataMarkup = createMarkup(formData);
     refs.formData.innerHTML = formDataMarkup;
     event.target.reset();
@@ -22,14 +28,35 @@ function handleSubmit(event) {
 
 function createMarkup(formData) {
     const { email, password, firstName, lastName, companyName } = formData;
+    if (companyName) {
+        return `
+            <p>Email: ${email}</p>
+            <p>Password: ${password}</p>
+            <p>First Name: ${firstName}</p>
+            <p>Last Name: ${lastName}</p>
+            <p>Company Name: ${companyName}</p>
+            `
+            }
     return `
     <p>Email: ${email}</p>
     <p>Password: ${password}</p>
     <p>First Name: ${firstName}</p>
     <p>Last Name: ${lastName}</p>
-    <p>Company Name: ${companyName}</p>
     `     
 };
 
+function validateEmail(email) {
+    const index = email.indexOf("@");
+    const slice = email.slice(index + 1, email.length);
+    if (slice.includes(".")) {
+        const newIndex = slice.indexOf(".")
+        const newSlice = slice.slice(newIndex + 1, slice.length)
+        if (!newSlice.includes(".")) {
+            console.log(newSlice);
+            return true;
+         }
+    }
+    return false;
+}
 
 
