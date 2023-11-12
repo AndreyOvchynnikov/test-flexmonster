386faacf965191b972d07c5f9f685e100374b56c
AndreyOvchynnikov <andrey272015@gmail.com>
2023-11-12 17:35:48 +0200

    update email validation

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const refs = {
     form: document.querySelector('[form]'),
     formDataContainer: document.querySelector('[form-data]'),
     backdrop: document.querySelector('[backdrop]'),
-    message: document.querySelector('[message]')
+    message: document.querySelector('[message]'),
   };
 
 refs.form.addEventListener('submit', handleSubmit);
@@ -32,8 +32,8 @@ function handleSubmit(event) {
         { name: "First Name", value: firstName.value },
         { name: "Last Name", value: lastName.value },
         { name: "Company Name", value: companyName.value },
-    ]
-    refs.formDataContainer.innerHTML = ""
+    ];
+    refs.formDataContainer.innerHTML = "";
     const formDataMarkup = createDataMarkup(formData);
     refs.formDataContainer.append(...formDataMarkup);
     event.target.reset();
@@ -43,39 +43,35 @@ function createDataMarkup(formData) {
     return formData.map(data => {
         const { name, value } = data
         if (!value) {
-            return
-        }
-        const element = document.createElement("p")
-        element.textContent = `${name}: ${value}`
-        return element
+            return;
+        };
+        const element = document.createElement("p");
+        element.textContent = `${name}: ${value}`;
+        return element;
     }).filter(el => el);     
 };
 
 function createMessageMarkup(message) {
-    const element = document.createElement("h2")
-    element.textContent = `${message}`
-    return element
+    const element = document.createElement("h2");
+    element.textContent = `${message}`;
+    return element;
 };
 
 function validateEmail(email) {
-    const regexCheck =  String(email).match(
-          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    const regexCheck = String(email).match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     );
-    const dogIndex = email.indexOf("@");
-    const dogSlice = email.slice(dogIndex + 1, email.length);
+    const pointIndex = email.indexOf(".");
+    const pointSlice = email.slice(pointIndex + 1, email.length);
 
-        if (regexCheck && dogSlice.includes(".")) {
-                const pointIndex = dogSlice.indexOf(".")
-                const pointSlice = dogSlice.slice(pointIndex + 1, dogSlice.length)
-                    
-                if (!pointSlice.includes(".") && pointSlice.length > 0) {
-                            return true;
-                }
-        }
+    if (regexCheck && !pointSlice.includes(".")) {
+        return true;
+    }
+    
     return false;
-}
+};
 
 function toggleMessage() {
     refs.backdrop.classList.toggle('is-hidden');
-  }
+};
 
